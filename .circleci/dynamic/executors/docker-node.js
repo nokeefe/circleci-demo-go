4a874f98d465c71ad6b1900823e75e3cef986727
nokeefe <nick.okeefe@cirlceci.com>
2023-04-19 15:06:13 -0700

    test: config sdk basic dynamic config

---
+++ .circleci/dynamic/executors/docker-node.js
@@ -0,0 +1,6 @@
+const CircleCI = require("@circleci/circleci-config-sdk");
+const dockerNode = new CircleCI.executors.DockerExecutor(
+    "cimg/node:lts",
+    "medium"
+);
+module.exports = dockerNode;
---
+++ .circleci/dynamic/index.js
@@ -0,0 +1,30 @@
+const CircleCI = require("@circleci/circleci-config-sdk");
+
+// Import Config Components
+const jobA = require("./jobs/jobA");
+const jobB = require("./jobs/jobB");
+
+// Initiate a new Config
+const myConfig = new CircleCI.Config()
+
+// Add elements to the config
+myConfig
+    .addJob(jobA)
+    .addJob(jobB)
+
+// Instantiate new Workflow and add jobA
+const dynamicWorkflow = new CircleCI.Workflow("dynamic-workflow");
+dynamicWorkflow.addJob(jobA);
+
+// Conditionally add jobB
+// This condition could be based on anything from the response of an API call
+// to the contents of a file
+if (1 == 1) {
+    dynamicWorkflow.addJob(jobB);
+}
+
+// Add the dynamic workflow to the config
+myConfig.addWorkflow(dynamicWorkflow);
+
+// Write the config to file
+myConfig.writeFile('dynamicConfig.yml');
