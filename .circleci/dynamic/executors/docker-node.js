1e4880a799373b97ea27ff36bce2695052d1a620
nokeefe <nick.okeefe@cirlceci.com>
2023-05-31 10:22:58 -0700

    test: test run

--- .circleci/dynamic/executors/docker-node.js
+++
@@ -1,6 +0,0 @@
-const CircleCI = require("@circleci/circleci-config-sdk");
-const dockerNode = new CircleCI.executors.DockerExecutor(
-    "cimg/node:lts",
-    "medium"
-);
-module.exports = dockerNode;
